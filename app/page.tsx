4178fea61849a05a091e3933bdac47795492a9ae
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2025-01-08 21:48:39 +0530

    document bug

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,18 @@ import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  const DynamicComponentWithNoSSR = dynamic(
-    () => import("@/components/hello3"),
-    { ssr: false }
-  );
+
+     const [docEnv, setDocEnv] = useState(false);
+
+    useEffect(() => {
+        if (typeof document !== "undefined") {
+            setDocEnv(true);
+        }
+    }, []);
+  
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
